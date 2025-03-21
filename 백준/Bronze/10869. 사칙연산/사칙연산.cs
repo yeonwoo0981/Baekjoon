8489ee24d5e8e5eaed872a804add9cf3b16d1ab9
ByulDaram <0981syw@naver.com>
2025-03-21 09:05:00 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,8 @@
+string[] input = Console.ReadLine().Split();
+int A = int.Parse(input[0]);
+int B = int.Parse(input[1]);
+Console.WriteLine(A + B);
+Console.WriteLine(A - B);
+Console.WriteLine(A * B);
+Console.WriteLine(A / B);
+Console.WriteLine(A % B);
