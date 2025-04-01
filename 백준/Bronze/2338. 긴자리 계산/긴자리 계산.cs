c7be8372aaa4f9e77c72653e19e6a624aff7d1c3
ByulDaram <0981syw@naver.com>
2025-04-01 16:51:47 +0900

    [Bronze V] Title: 긴자리 계산, Time: 52 ms, Memory: 5696 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.cs
@@ -0,0 +1,6 @@
+using System.Numerics;
+BigInteger A = BigInteger.Parse(Console.ReadLine());
+BigInteger B = BigInteger.Parse(Console.ReadLine());
+Console.WriteLine(A + B);
+Console.WriteLine(A - B);
+Console.WriteLine(A * B);
