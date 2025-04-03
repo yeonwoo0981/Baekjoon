4a83738c0e6601a269630a17ec9b49009a0a0e6d
ByulDaram <0981syw@naver.com>
2025-04-03 16:58:48 +0900

    [Bronze V] Title: CASIO, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cs
@@ -0,0 +1,11 @@
+string[] str = Console.ReadLine().Split();
+long N = long.Parse(str[0]);
+long M = long.Parse(str[1]);
+if (N == M)
+{
+    Console.WriteLine('1');
+}
+else
+{
+    Console.WriteLine('0');
+}
