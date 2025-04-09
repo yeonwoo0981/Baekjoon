0268aba5dba9757e80a9105f5e921c2ed3d15b31
ByulDaram <0981syw@naver.com>
2025-04-10 08:37:12 +0900

    [Bronze V] Title: Plane, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cs
@@ -0,0 +1,6 @@
+string[] str = Console.ReadLine().Split();
+int n = int.Parse(str[0]);
+int k = int.Parse(str[1]);
+int N = int.Parse(str[2]);
+int K = int.Parse(str[3]);
+Console.WriteLine(n * k + N * K);
