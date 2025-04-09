96001db4b1be042bacc1d2d9983b8daf9225da57
ByulDaram <0981syw@naver.com>
2025-04-09 12:59:00 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cs
@@ -0,0 +1,12 @@
+ while (true)
+ {
+     string[] str = Console.ReadLine().Split();
+     int M = int.Parse(str[0]);
+     int F = int.Parse(str[1]);
+     if (M == 0 && F == 0)
+     
+         break;
+
+         Console.WriteLine(M + F);
+     
+ }
