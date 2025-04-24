3d227679ad68cd73e8bbc60c91a322369e618141
ByulDaram <0981syw@naver.com>
2025-04-25 08:52:08 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 44 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cs
@@ -0,0 +1,5 @@
+int[] a = { 3, 4, 5 };
+int r = int.Parse(Console.ReadLine());
+int g = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(r * a[0] + g * a[1] + b * a[2]);
