db093fb97c04052ba4dbc60ec7cead8a3c5af191
ByulDaram <0981syw@naver.com>
2025-06-10 16:29:43 +0900

    [Bronze III] Title: 학점계산, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.cs
@@ -0,0 +1,53 @@
+string c = Console.ReadLine();
+if (c == "A+")
+{
+    Console.WriteLine("{0:F1}", 4.3);
+}
+else if (c == "A0")
+{
+    Console.WriteLine("{0:F1}", 4.0);
+}
+else if (c == "A-")
+{
+    Console.WriteLine("{0:F1}", 3.7);
+}
+else if (c == "B+")
+{
+    Console.WriteLine("{0:F1}", 3.3);
+}
+else if (c == "B0")
+{
+    Console.WriteLine("{0:F1}", 3.0);
+}
+else if (c == "B-")
+{
+    Console.WriteLine("{0:F1}", 2.7);
+}
+else if (c == "C+")
+{
+    Console.WriteLine("{0:F1}", 2.3);
+}
+else if (c == "C0")
+{
+    Console.WriteLine("{0:F1}", 2.0);
+}
+else if (c == "C-")
+{
+    Console.WriteLine("{0:F1}", 1.7);
+}
+else if (c == "D+")
+{
+    Console.WriteLine("{0:F1}", 1.3);
+}
+else if (c == "D0")
+{
+    Console.WriteLine("{0:F1}", 1.0);
+}
+else if (c == "D-")
+{
+    Console.WriteLine("{0:F1}", 0.7);
+}
+else if (c == "F")
+{
+    Console.WriteLine("{0:F1}", 0.0);
+}
