71ff2b20ec899490f1d2c232555f038c3e0b0463
ByulDaram <0981syw@naver.com>
2025-07-28 00:41:05 +0900

    [Bronze V] Title: 아!, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cs
@@ -0,0 +1,10 @@
+string a = Console.ReadLine();
+string h = Console.ReadLine();
+if (a.Length >= h.Length)
+{
+    Console.WriteLine("go");
+}
+else if (a.Length < h.Length)
+{
+    Console.WriteLine("no");
+}
