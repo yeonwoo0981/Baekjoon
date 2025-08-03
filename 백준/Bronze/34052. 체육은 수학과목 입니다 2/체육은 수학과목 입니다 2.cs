edc3f8278e07a3ebbaa48407222fc7075debd3e5
ByulDaram <0981syw@naver.com>
2025-08-03 22:20:32 +0900

    [Bronze V] Title: 체육은 수학과목 입니다 2, Time: 48 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/34052. 체육은 수학과목 입니다 2/체육은 수학과목 입니다 2.cs
@@ -0,0 +1,6 @@
+int finalRun = 300;
+int firstRun = int.Parse(Console.ReadLine());
+int secondRun = int.Parse(Console.ReadLine());
+int thirdRun = int.Parse(Console.ReadLine());
+int fourthRun = int.Parse(Console.ReadLine());
+Console.WriteLine(firstRun + secondRun + thirdRun + fourthRun + finalRun <= 1800 ? "Yes" : "No");
