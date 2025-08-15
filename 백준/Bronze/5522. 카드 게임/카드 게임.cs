e5e73879ee85203a45b2dbb67cfb846e866830a7
ByulDaram <0981syw@naver.com>
2025-08-16 00:53:39 +0900

    [Bronze V] Title: 카드 게임, Time: 48 ms, Memory: 5472 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cs
@@ -0,0 +1,6 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b + c + d + e);
