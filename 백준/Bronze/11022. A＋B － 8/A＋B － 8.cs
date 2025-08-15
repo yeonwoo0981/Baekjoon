ab843b83b5324ef1bb2611025a9daff392301818
ByulDaram <0981syw@naver.com>
2025-08-16 01:00:14 +0900

    [Bronze V] Title: A+B - 8, Time: 56 ms, Memory: 6192 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,9 @@
+ int T = int.Parse(Console.ReadLine());
+ for (int x = 1; x <= T; x++)
+ {               
+     string[] str = Console.ReadLine().Split();
+     int A = int.Parse(str[0]);
+     int B = int.Parse(str[1]);
+     int C = A + B;
+     Console.WriteLine($"Case #{x}: {A} + {B} = {C}");
+ }
