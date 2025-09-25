2dbb7493bcb3f0f9eb38a55158269157bb6d5b90
ByulDaram <0981syw@naver.com>
2025-09-25 19:02:39 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int n, k = 0;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		b = k;
+		cin >> a >> b;
+	}
+	cout << "비와이" << endl;
+	return 0;
+}
